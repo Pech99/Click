b17e3e120d297611e9eab85ba28b17770fa347ca
Pecce <vittorio1999@live.com>
2022-10-22 10:47:40 +0200

    VK conv

--- click.go
+++ click.go
@@ -1,5 +1,40 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"runtime"
+	"time"
+
+	"github.com/micmonay/keybd_event"
+)
+
+// .\click.exe <ctrl> <alt> <canc>
+
 func main() {
 
+	fmt.Println(os.Args)
+
+	kb, err := keybd_event.NewKeyBonding()
+	if err != nil {
+		panic(err)
+	}
+
+	if runtime.GOOS == "linux" {
+		time.Sleep(2 * time.Second)
+	}
+
+	VK.conv["<A>"]
+
+	kb.SetKeys(keybd_event.VK_A, keybd_event.VK_B)
+	kb.HasSHIFT(true)
+
+	err = kb.Launching()
+	if err != nil {
+		panic(err)
+	}
+
+	kb.Press()
+	time.Sleep(10 * time.Millisecond)
+	kb.Release()
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Pech99/Click
 
 go 1.19
+
+require github.com/micmonay/keybd_event v1.1.1 // indirect
